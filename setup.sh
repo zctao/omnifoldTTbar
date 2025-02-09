a263aa5b6402a6ce64a96e11628dfa26eec0a59b
Zhengcheng Tao <zhengcheng.tao@cern.ch>
2025-02-09 00:11:46 -0800

    Rename cedar/ to slurm/

--- setup.sh
+++ setup.sh
@@ -4,4 +4,4 @@ export DATA_DIR=${1:-$HOME/data}
 export SOURCE_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
 # cf. https://stackoverflow.com/questions/59895/how-to-get-the-source-directory-of-a-bash-script-from-within-the-script-itself
 
-export PYTHONPATH=${SOURCE_DIR}/python:${SOURCE_DIR}/scripts:${SOURCE_DIR}/cedar:${SOURCE_DIR}:$PYTHONPATH
+export PYTHONPATH=${SOURCE_DIR}/python:${SOURCE_DIR}/scripts:${SOURCE_DIR}/slurm:${SOURCE_DIR}:$PYTHONPATH
